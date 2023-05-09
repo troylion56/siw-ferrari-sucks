155d446ea6d30ff6ae276a458cb300b1ad7a108b
troylion56 <bluphoenix56@gmail.com>
2023-05-09 21:41:52 +0200

    cambiare l'import mettere javax

--- src/main/java/it/unitoma3/ferrarisucks/controller/indexController.java
+++ src/main/java/it/unitoma3/ferrarisucks/controller/indexController.java
@@ -1,9 +1,9 @@
 package it.unitoma3.ferrarisucks.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import ch.qos.logback.core.model.Model;
 
 @Controller
 public class indexController {
